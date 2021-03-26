d68a97f5104c38edaeab0e69b42bfb257f999ad1
Andreas <andreas.rau@hs-furtwangen.de>
2021-03-26 10:13:54 +0100

    tsdsfs

--- Task2/script.js
+++ Task2/script.js
@@ -1,3 +1,20 @@
 "use strict";
-console.log("Hallo Welt test");
+console.log("Hallo Welt");
+console.log("oh no");
+function a1() {
+    let potatoe = "ich bin eine kartoffel";
+    if (potatoe == "ich bin eine kartoffel")
+        return potatoe;
+    return "hall";
+}
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+function cheese(name) {
+    let user = name;
+    console.log(user);
+}
 //# sourceMappingURL=script.js.map
--- Task2/script.ts
+++ Task2/script.ts
@@ -1,1 +1,17 @@
 console.log("Hallo Welt");
+console.log("oh no");
+
+
+function a1(): string {
+    let potatoe: string = "ich bin eine kartoffel";
+    if (potatoe == "ich bin eine kartoffel")
+        return potatoe;
+    
+    return "hall";
+}
+enum Color {Red, Green, Blue}
+
+function cheese(name: string): void {
+    let user: string = name;
+    console.log(user);
+}
